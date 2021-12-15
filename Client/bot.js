50f5de50f1eeae7b35750e71374ebeda15cde654
kaeky <carlosbasan2011@hotmail.com>
2021-12-15 01:49:11 -0500

    gitignore

--- Client/bot.js
+++ Client/bot.js
@@ -5,23 +5,23 @@ const { generateDependencyReport } = require('@discordjs/voice');
 const { Intents } = require('discord.js');
 const { GCommandsClient } = require("gcommands")
 
-const client = new GCommandsClient({
-    loader: {
-        cmdDir: __dirname + "/commands/",
-    },
-    language: "english",
-    commands: {
-        slash: 'both',
-        context: false,
-        prefix: "*"
-    },
-    intents: [["GUILDS","GUILD_MESSAGES"],
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
+// const client = new GCommandsClient({
+//     loader: {
+//         cmdDir: __dirname + "/commands/",
+//     },
+//     language: "english",
+//     commands: {
+//         slash: 'both',
+//         context: false,
+//         prefix: "*"
+//     },
+//     intents: [["GUILDS","GUILD_MESSAGES"],
+//         Intents.FLAGS.GUILDS,
+//         Intents.FLAGS.GUILD_VOICE_STATES,
+//         Intents.FLAGS.GUILD_MEMBERS
+//     ]
 
-})
+// })
 
 function start () {
     console.log(generateDependencyReport());
