9d8601f6b03fdd0f8410887013ff3af3d9fb5c39
kaeky <carlosbasan2011@hotmail.com>
2021-12-15 02:19:57 -0500

    hola

--- Client/bot.js
+++ Client/bot.js
@@ -1,50 +1,50 @@
 require('dotenv').config();
-// const Discord = require("discord.js");
-// const { generateDependencyReport } = require('@discordjs/voice');
-// //const { Intents } = require('discord.js');
-// //const { GCommandsClient } = require("gcommands")
-// //const client = new Discord.client();
-// // const client = new GCommandsClient({
-// //     loader: {
-// //         cmdDir: __dirname + "/commands/",
-// //     },
-// //     language: "english",
-// //     commands: {
-// //         slash: 'both',
-// //         context: false,
-// //         prefix: "*"
-// //     },
-// //     intents: [["GUILDS","GUILD_MESSAGES"],
-// //         Intents.FLAGS.GUILDS,
-// //         Intents.FLAGS.GUILD_VOICE_STATES,
-// //         Intents.FLAGS.GUILD_MEMBERS
-// //     ]
+const Discord = require("discord.js");
+const { generateDependencyReport } = require('@discordjs/voice');
+const { Intents } = require('discord.js');
+const { GCommandsClient } = require("gcommands")
+const client = new Discord.client();
+const client = new GCommandsClient({
+    loader: {
+        cmdDir: __dirname + "/commands/",
+    },
+    language: "english",
+    commands: {
+        slash: 'both',
+        context: false,
+        prefix: "*"
+    },
+    intents: [["GUILDS","GUILD_MESSAGES"],
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
 
-// // })
+})
 
-// function start () {
-//     console.log(generateDependencyReport());
-//     //client
-//         //.on("log", console.log)
-//        // .on("debug", console.log)
-//     //client.login(process.env.DISCORD_TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
+function start () {
+    console.log(generateDependencyReport());
+    client
+        .on("log", console.log)
+       .on("debug", console.log)
+    client.login(process.env.DISCORD_TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
 
 
     
-// }
-const Discord = require("discord.js");
-const { AudioPlayerStatus } = require("@discordjs/voice");
-const client = new Discord.Client();
-const { generateDependencyReport } = require('@discordjs/voice');
-
-function start () {
-
-    console.log(generateDependencyReport());
-    client
-    .on("log", console.log)
-    .on("debug", console.log)
-    client.login(config.TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
-    // messageUpdate.messageUpdate(client)
-    // mainMessage.mainMessage(client, prefix, Discord,joinVoiceChannel) 
 }
+// const Discord = require("discord.js");
+// const { AudioPlayerStatus } = require("@discordjs/voice");
+// const client = new Discord.Client();
+// const { generateDependencyReport } = require('@discordjs/voice');
+
+// function start () {
+
+//     console.log(generateDependencyReport());
+//     client
+//     .on("log", console.log)
+//     .on("debug", console.log)
+//     client.login(process.env.DISCORD_TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
+//     // messageUpdate.messageUpdate(client)
+//     // mainMessage.mainMessage(client, prefix, Discord,joinVoiceChannel) 
+// }
 module.exports = {start}
