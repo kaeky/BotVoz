ffc00e485133cea6050108c17c3d65b3e67375b6
kaeky <carlosbasan2011@hotmail.com>
2021-12-14 23:21:46 -0500

    first

---
+++ Client/bot.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const config = require('../config.json');
+const { generateDependencyReport } = require('@discordjs/voice');
+const { Intents } = require('discord.js');
+const { GCommandsClient } = require("gcommands")
+
+const client = new GCommandsClient({
+    loader: {
+        cmdDir: __dirname + "/commands/",
+    },
+    language: "english",
+    commands: {
+        slash: 'both',
+        context: false,
+        prefix: "*"
+    },
+    intents: [["GUILDS","GUILD_MESSAGES"],
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
+
+})
+
+client.config = config;
+
+function start () {
+    console.log(generateDependencyReport());
+    client
+        .on("log", console.log)
+        .on("debug", console.log)
+    client.login(config.TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
+//    //ready.inicia(client, Discord)
+
+    
+}
+
+module.exports = {start}
