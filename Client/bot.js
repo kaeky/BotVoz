5cee6ebcfe7a2e4f069f3841600803ba72d91b67
kaeky <carlosbasan2011@hotmail.com>
2021-12-15 00:03:06 -0500

    gitignore

--- Client/bot.js
+++ Client/bot.js
@@ -1,34 +1,32 @@
 require('dotenv').config();
-const Discord = require("discord.js");
-const config = require('../config.json');
+//const Discord = require("discord.js");
+//const app = new Discord.Client();
 const { generateDependencyReport } = require('@discordjs/voice');
-const { Intents } = require('discord.js');
-const { GCommandsClient } = require("gcommands")
+//const { Intents } = require('discord.js');
+//const { GCommandsClient } = require("gcommands")
 
-const client = new GCommandsClient({
-    loader: {
-        cmdDir: __dirname + "/commands/",
-    },
-    language: "english",
-    commands: {
-        slash: 'both',
-        context: false,
-        prefix: "*"
-    },
-    intents: [["GUILDS","GUILD_MESSAGES"],
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
 
-})
+// const client = new GCommandsClient({
+//     loader: {
+//         cmdDir: __dirname + "/commands/",
+//     },
+//     language: "english",
+//     commands: {
+//         slash: 'both',
+//         context: false,
+//         prefix: "*"
+//     },
+//     intents: [["GUILDS","GUILD_MESSAGES"],
+//         Intents.FLAGS.GUILDS,
+//         Intents.FLAGS.GUILD_VOICE_STATES,
+//         Intents.FLAGS.GUILD_MEMBERS
+//     ]
+
+// })
 
 function start () {
     console.log(generateDependencyReport());
-    client
-        .on("log", console.log)
-        .on("debug", console.log)
-    client.login(process.env.DISCORD_TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
+
 
 
     
